9bd5f08567d86a0508cb0c44a69e82fcf5464a3a
Rodrigo Pierre <rodrigopierre05@gmail.com>
2024-05-28 22:31:28 -0300

    conectei os checkboxes com os states

--- password-generate/src/App.jsx
+++ password-generate/src/App.jsx
@@ -11,10 +11,10 @@ class App extends React.Component{
       len:10,
       password:"P4$5W0rD!", 
       strength:0,
-      lowercase:true,
-      uppercase:true,
-      numbers:true,
-      symbols:true};
+      lowercase:false,
+      uppercase:false,
+      numbers:false,
+      symbols:false};
   }
 
   // função que lida com o clique no botão "generate" e atualiza a força da senha
@@ -29,6 +29,52 @@ class App extends React.Component{
   // para trocar a cor do placeholder pela da senha ao gerar a primeira vez
     document.getElementById('password').style.color = '#dadada';
   }
+  
+  // conexão dos states com os checkboxes
+  clique_uppercase = 0;       //uppercase
+  chk_Uppercase = () => {
+    if (this.clique_uppercase == 0){
+      this.setState({ uppercase:true });
+      this.clique_uppercase = 1;
+    } else {
+      this.setState({ uppercase:false });
+      this.clique_uppercase = 0
+    }
+  }
+
+  clique_lowercase = 0;       //lowercase
+  chk_Lowercase = () => {
+    if (this.clique_lowercase == 0){
+      this.setState({ lowercase:true });
+      this.clique_lowercase = 1;
+    } else {
+      this.setState({ lowercase:false });
+      this.clique_lowercase = 0
+    }
+  }
+
+  clique_numbers = 0;        //numbers
+  chk_Numbers = () => {
+    if (this.clique_numbers == 0){
+      this.setState({ numbers:true });
+      this.clique_numbers = 1;
+    } else {
+      this.setState({ numbers:false });
+      this.clique_numbers = 0
+    }
+  }
+
+  clique_symbols = 0;        //symbols
+  chk_Symbols = () => {
+    if (this.clique_symbols == 0){
+      this.setState({ symbols:true });
+      this.clique_symbols = 1;
+    } else {
+      this.setState({ symbols:false });
+      this.clique_symbols = 0
+    }
+  }
+
 
   render(){
     return (
@@ -40,7 +86,7 @@ class App extends React.Component{
         <div className='screen-middle'>
           {/* tipo aqui embaixo é uma referencia ao state agora */}
           <h2 id='password'>{this.state.password}</h2>
-          <button className='img-btn'><img src="\public\img-copiar.png" alt="img" className='img' /></button>
+          <button className='img-btn'><img src="\img-copiar.png" alt="img" className='img' /></button>
         </div>
   
         {/* todas as classes de caracter eu coloquei em português, porque acho difícil escrever em inglês */}
@@ -55,19 +101,19 @@ class App extends React.Component{
             </div>
           </div>
           <div className='includes'>
-            <input type="checkbox" className='chk' />
+            <input type="checkbox" className='chk' onClick={this.chk_Uppercase} />
             <p>Include Uppercase Letters</p>
           </div>
           <div className='includes'>
-            <input type="checkbox" className='chk' />
+            <input type="checkbox" className='chk' onClick={this.chk_Lowercase} />
             <p>Include Lowercase Letters</p>
           </div>
           <div className='includes'>
-            <input type="checkbox" className='chk' />
+            <input type="checkbox" className='chk' onClick={this.chk_Numbers} />
             <p>Include Numbers</p>
           </div>
           <div className='includes'>
-            <input type="checkbox" className='chk' />
+            <input type="checkbox" className='chk' onClick={this.chk_Symbols} />
             <p>Include Symbols</p>
           </div>
           <div className='strenght'>
